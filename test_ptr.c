2e51069b0856fe05289e8f45aa0ead2364a1faa2
wayne777Chiu <homeworker.wayne@gmail.com>
2018-06-27 18:29:08 +0800

    cut the redundancy code

--- test_ptr.c
+++ test_ptr.c
@@ -43,26 +43,4 @@ void main()
         printf("c's address read by &c: %p, *&ptr1: %p, &*ptr1: %p, *ptr2: %p\n",&c, *&ptr1,&*ptr1,*ptr2);
 
         printf("ptr1 address read by &ptr1: %p, &*ptr2: %p, *&ptr2: %p\n", &ptr1,&*ptr2,*&ptr2);
-
-/*        
-printf("Once ptr is set (int **ptr),\n");
-	printf("this is just without initialization data\n");
-	printf("ptr address will be %p\n",&ptrC);
-	printf("*ptr will get another address(*ptr) %p(context)\n",*ptrC);
-	printf(" in ptr context will be another address %p\n",&*ptrC);
-        printf("after we give the value. ex. c=6; *ptrC = &c \n");
-	*ptrC = &c;
-	**ptrD = 8;
-	printf("**ptrD should be *ptrD %p \n",*ptrD);
-	printf("*ptr should be variable c's address if c address is %p then **ptr should be the value c=6 %d\n",&c, **ptrC);
-	printf("ptrA(context) addr: %p\n",ptrA);
-	printf("a addr: %p\n",&a);
-	printf("b addr: %p\n",&b);
-	func(&ptrA);
-	printf("ptrB addr: %p\n",&ptrB);
-	printf("ptrA addr: %p\n",&ptrA);
-	*&ptrB = &a;
-	printf("ptrB(context): %p\n",ptrB);
-	printf("the result *ptra: %d\n",*ptrA);
-*/
 }
