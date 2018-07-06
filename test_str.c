b8cc9c4de1f1e6573c578ff11843cd481bc47c2a
wayne777Chiu <homeworker.wayne@gmail.com>
2018-07-06 18:32:31 +0800

    update

--- test_str.c
+++ test_str.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 void one_ptr(char *s);
 void diff_ptr_n_array(char *ptr, char *arrptr);
@@ -47,8 +48,8 @@ void diff_ptr_n_array(char *ptr, char *arrptr)
     printf("FUNCTION: %s \n",__func__);
     printf("In this part, we explain the different between pointer string & array string\n");
     printf("We set two string to descript:\n");
-    printf("A: pointer string (char *ptr_string=\"abcd\";)\n");
-    printf("B: array string   (char arr_string[4]= \"ABCD\";) (arrptr)\n");
+    printf("A: pointer string (char *ptr_string=\"abcd\";): %s\n",ptr);
+    printf("B: array string   (char arr_string[4]= \"ABCD\";) (arrptr): %s\n", arrptr);
     printf("1. Change one char ex. array[1], *(ptr+1)\n");
     printf("   Let the char on index 1 position to become '2'\n");
    
@@ -64,26 +65,59 @@ void diff_ptr_n_array(char *ptr, char *arrptr)
     printf("If you use the *ptr as string for clarm,\n");
     printf("you will got error. ex. below is sample\n");     
     printf("f: ptr_string[1] = '2'; \n");
+    printf("Or\n");
+    printf("f: *(ptr +1) = '3'\n");
 #if 0
     ptr[1] = '2';
     printf(" printf the string (ptr): %s\n", ptr);
-#endif
-    printf("Error: Segmentation fault (core dumped)\n");
 
-    printf("If you use the *ptr as string for clarm,\n");     
-    printf("you will got error. ex. below is sample\n"); 
-    printf("f: *(ptr +1) = '3'\n");
-#if 0
     *(ptr + 1) = '3';
     printf(" printf the string (ptr): %s\n", ptr);
 #endif
-    printf("Error: Segmentation fault (core dumped)\n");
+    printf("We got same result: (Segmentation fault)\n");
+    printf("!!!Error: Segmentation fault (core dumped)\n");
+    
+    char *ptr2 = ptr;
+    char *ptr3 = "HDMI";
+    *ptr++ = *ptr3++;
+    printf("rsult is %s\n",ptr2);
+
+    printf("another method Dynamic string (malloc) \n");
+    char *dptr = malloc(strlen(ptr));
+    dptr = "EFGH";
+    *(dptr+1) = '2';
+    printf("print the string(dptr):%s \n",dptr);
+
 
     printf("But if we really want to update\n");
-    char **pptr;
-    pptr = &(s+1);
-    dxsce
+    printf("set %s \n", ptr+1);    
+#if 1
+    printf("\test lenght");
+    char hel[10] = "2345";
+    char *helptr ="4567";
+    int len1 = strlen(hel),len2= strlen(helptr);
+    printf("length %s %d %s %d\n", hel, len1, helptr, len2);
+ 
+char **pptr;
+char *aptr1;
+    aptr1 = ptr;
+    ptr = ptr+1;
+    printf("23423\n");
+    pptr = &(ptr);
+    //aptr1[0] = 'c';
+    printf(" ptr char one %c\n",ptr[0]);   
+    printf("1 pptr %s \n",*pptr);
+    *pptr = "cc";
 
+    printf("1 pptr %s \n",*pptr);
+  
+//  **ptr = '3'; 
+    printf("2\n");
+//    printf(" the result s %s\n",*ptr);
+    char c ='\n';
+    printf(" \\0 printf %c\n",c);
+    printf("3\n");
+#endif
 }
 
 void one_ptr(char *s)
